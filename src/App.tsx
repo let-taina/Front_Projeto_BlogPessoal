64badda0a382824584079d0f1dc8ccd7c250238a
let-taina <le_taina@hotmail.com>
2024-06-30 18:19:29 -0300

    Exemplo de props e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-/*componente de função chamado App.tsx 
-dentro do return um seletor de Home.tsx e import do caminho do Home.tsx para dentro do arquivo*/
-
-import React from 'react';
 import Home from './pages/home/Home';
 import './App.css';
-
+import React from 'react';
 
 function App() {
+ 
   return (
-   <Home />
+    <>
+      <Home/>
+    </>
   );
 }
 
 export default App;
+
