dba25ba09057cfaa51f684a32084c9da1ce688e1
let-taina <le_taina@hotmail.com>
2024-06-30 23:26:31 -0300

    criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import Home from './pages/home/Home';
-import './App.css';
 import React from 'react';
-
+import './App.css';
+import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 function App() {
- 
   return (
     <>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
-
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom'
+import React from 'react'
 
-function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
 
-  return (
-    <div>
-      {loggedIn ? (
-        <h1>Bem-vindo de volta!</h1>
-      ) : (
-        <button onClick={() => setLoggedIn(true)}>Entrar</button>
-      )}
-    </div>
-  );
+
+const Home = () => {
+    let navigate = useNavigate()
+    return (
+
+        <div>
+            <h2 className="text-slate-900 text-5xl  m-4">Home</h2>
+            <div>
+                <button type='submit'
+                    className='hover:underline mx-4'
+                    onClick={() => { navigate('/login') }}>
+                    Login useNavigate
+                </button>
+                <Link to='/login' className='hover:underline mx-4'>Login por Link</Link>
+            </div>
+
+        </div>
+
+    )
 }
-export default Home;
+
+export default Home
