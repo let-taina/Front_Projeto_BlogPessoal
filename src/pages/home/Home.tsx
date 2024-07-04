8407e88a2519fb677a67593d0851866f22f06fcc
let-taina <le_taina@hotmail.com>
2024-07-04 14:44:37 -0300

    Template do form postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import homeLogo from '../../assets/home.png'
 import './Home.css';
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 
 function Home() {
   return (
@@ -13,7 +14,7 @@ function Home() {
             <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
 
             <div className="flex justify-around gap-4">
-
+            <ModalPostagem />
               <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
             </div>
           </div>
