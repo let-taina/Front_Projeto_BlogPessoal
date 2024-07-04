753a4ca2a4d0b5baf96d9f77ba6e938832a3b177
let-taina <le_taina@hotmail.com>
2024-07-04 17:03:58 -0300

    Componente perfil

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -17,25 +17,30 @@ function Navbar() {
 
   let navbarComponent
   
-
-  return (
-    <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+  if(usuario.token !== "") {
+    navbarComponent = (
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link> 
 
             <div className='flex gap-4'>
             <Link to='/postagens' className='hover:underline'>Postagens</Link>
-            <Link to='/temas' className='hover:underline'>Temas</Link>
-            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-              <div className='hover:underline'>Perfil</div>
+              <Link to='/temas' className='hover:underline'>Temas</Link>
+              <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+              <Link to='/perfil' className='hover:underline'>Perfil</Link>
               <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
              
             </div>
           </div>
         </div>
-    </>
+    
   )
+}
+return (
+  <>
+    {navbarComponent}
+  </>
+)
 }
 
 export default Navbar
