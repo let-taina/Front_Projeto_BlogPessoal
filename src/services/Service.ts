89d98ad6f13c9f12169a59ccb9db340f469bde59
let-taina <le_taina@hotmail.com>
2024-07-10 14:44:45 -0300

    Deploy do projeto blog

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,8 +2,8 @@
 
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'https://blogpessoal-yo1c.onrender.com/'
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
